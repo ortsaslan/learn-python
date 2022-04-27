3e7110c74176b048d349fa3df25e901db3629f20
Aslan Aliev <aslan.a.aliev@gmail.com>
2022-04-27 16:07:21 +0300

    add: logic (user input and compare to word)

--- for-beginners/hangman/hangman.py
+++ for-beginners/hangman/hangman.py
@@ -1,2 +1,15 @@
+import random
+
 print("H A N G M A N")
-print("The game will be available soon.")
+
+words = ["python", "java", "swift", "javascript"]
+
+while True:
+    random.seed()
+    word = random.choice(words)
+    user_in = input("Guess the word: ")
+    if user_in == word:
+        print("You survived!")
+        break
+    else:
+        print("You lost!")
