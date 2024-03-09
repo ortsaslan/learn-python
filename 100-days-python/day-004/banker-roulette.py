b4a3d3b07a96f1515407845404a36dd76dabe508
Aslan Aliev <ortsaslan@gmail.com>
2024-03-09 21:23:19 +0300

    refactor: add var person_who_pays

--- 100-days-python/day-004/banker-roulette.py
+++ 100-days-python/day-004/banker-roulette.py
@@ -6,11 +6,14 @@ names_str = input("Type names, separated by a comma and space: ")
 # Split string to list of names
 names_lst = names_str.split(", ")
 
-# Defigning upper bound for random numbers generator
+# Defining upper bound for random numbers generator
 generator_ceil = len(names_lst) - 1
 
 # Finding index randomly to choose a name
 index = random.randint(0, generator_ceil)
 
-# Print name from names list
-print(f"{names_lst[index]} is going to buy the meal today!")
+# Define person who will pay
+person_who_pays = names_lst[index]
+
+# Print name of person who pays
+print(f"{person_who_pays} is going to buy the meal today!")
