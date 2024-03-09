ff85964693d15136b745274bf925d5a7b7bad8b1
Aslan Aliev <ortsaslan@gmail.com>
2024-03-09 22:03:58 +0300

    add: app file treasure-map.py

---
+++ 100-days-python/day-004/treasure-map.py
@@ -0,0 +1,16 @@
+# Define rows and map
+row1 = ["⬜️", "⬜️", "⬜️"]
+row2 = ["⬜️", "⬜️", "⬜️"]
+row3 = ["⬜️", "⬜️", "⬜️"]
+map = [row1, row2, row3]
+
+# Print a map
+print(f"{row1}\n{row2}\n{row3}")
+
+# Take position and put into it a treasure
+position_input = input("Where do you want to put the treasure? Type two-digit number, first for row and second for column: ")
+position = [int(position_input[0]) - 1, int(position_input[1]) - 1]
+map[position[0]][position[1]] = "❎"
+
+# Print map with treasure position
+print(f"{map[0]}\n{map[1]}\n{map[2]}")
