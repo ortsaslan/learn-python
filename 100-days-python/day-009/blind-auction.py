22232a08e89a347db84edec0e1ec3ade5df417f4
Aslan Aliev <ortsaslan@cube.local>
2024-03-15 17:23:47 +0300

    feat: implement app's logic

--- 100-days-python/day-009/blind-auction.py
+++ 100-days-python/day-009/blind-auction.py
@@ -9,3 +9,33 @@
 ### compare current item's key and value with previous ones, store highest k/v
 ### if current item is first store key/value in vars
 ## return winner's Name and Bid
+
+# Define dictionary to store bidders data
+bidders_and_bids = dict()
+
+# Take bidders data from stdin
+while True:
+    name = input("\nEnter your name: ")
+    bid = int(input("\nEnter your bid: $"))
+
+    bidders_and_bids[name] = bid
+
+    next_bidder = input("\nAre there any bidders? Type 'yes' or 'no': ")
+    if next_bidder == "no":
+        break
+    continue
+
+# Compare bids, define highest one
+name = ""
+bid = 0
+for key in bidders_and_bids:
+    if name == "" and bid == 0:
+        name = key
+        bid = bidders_and_bids[key]
+    else:
+        if bidders_and_bids[key] > bid:
+            name = key
+            bid = bidders_and_bids[key]
+
+# Return winners data
+print(f"\nThe winner is {name} with a bid of ${bid}.")
