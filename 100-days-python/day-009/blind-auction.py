c52d3d2a779e669445f9ef6b092eeae5b9f8b816
Aslan Aliev <ortsaslan@cube.local>
2024-03-15 15:12:49 +0300

    add: app file blind-auction.py

---
+++ 100-days-python/day-009/blind-auction.py
@@ -0,0 +1,11 @@
+# Program algorithm:
+## Take and store Name and Bid of bidders from stdin.
+## Compare stored bids and define the highest bid.
+## Print out Name ang Bid of person with highest bid.
+
+# Program realization:
+## Define dictionary to store name:bid pair.
+## Iterate over dictionary with for-loop:
+### compare current item's key and value with previous ones, store highest k/v
+### if current item is first store key/value in vars
+## return winner's Name and Bid
